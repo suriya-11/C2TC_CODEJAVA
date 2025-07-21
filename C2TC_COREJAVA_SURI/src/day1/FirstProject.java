fc65da75ab1d427a88cd43003cd9d70070a3bc6a
ETHNOTECH <ETHNOTECH@DESKTOP-7KKSNUC>
2025-07-21 12:25:35 +0530

    first java program

---
+++ C2TC_COREJAVA_SURI/src/day1/FirstProject.java
@@ -0,0 +1,12 @@
+package day1;
+
+public class FirstProject {
+
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub
+		System.out.print("Welcome to Java class");
+
+	}
+
+}
