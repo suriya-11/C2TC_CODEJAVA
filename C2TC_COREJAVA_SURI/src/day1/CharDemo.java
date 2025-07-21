aaf8a2f6e75c5d29ebfe986cc0eb107bf1b9017e
suriya-11 <suryasuriya716@gmail.com>
2025-07-21 21:38:00 +0530

    Create CharDemo.java

---
+++ C2TC_COREJAVA_SURI/src/day1/CharDemo.java
@@ -0,0 +1,16 @@
+package com.tnsif.dayone;
+
+public class CharDemo {
+
+    public static void main(String[] args) {
+        char ch = 'A';
+        char ch1 = 66; // ASCII value of 'B'
+        int var1 = ch;
+        int var2 = ch1;
+
+        System.out.println(ch);    // prints: A
+        System.out.println(ch1);   // prints: B
+        System.out.println(var1);  // prints: 65
+        System.out.println(var2);  // prints: 66
+    }
+}
