f7f535bc1badc8eceb4f455ea583df2d49cc36d0
HP <HP@DESKTOP-Q9DA9LP>
2025-08-01 10:03:10 +0530

    day12

---
+++ c2tc_corejava/src/day12/InvalidMarksException.java
@@ -0,0 +1,11 @@
+package day12;
+
+public class InvalidMarksException extends Exception{
+
+	public InvalidMarksException() {
+		super();
+	}
+
+	public InvalidMarksException(String message) {
+		super(message);
+	}
