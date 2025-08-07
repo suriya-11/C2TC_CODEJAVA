c7a851201172b6061098cdea85ec1feb1862c012
HP <HP@DESKTOP-Q9DA9LP>
2025-08-07 09:41:26 +0530

    day15

---
+++ c2tc_corejava/src/day15/generics/UnBoundedWildCard.java
@@ -0,0 +1,12 @@
+package day15.generics;
+
+import java.util.List;
+
+public class UnBoundedWildCard {
+	//Parameter List(<?>) is List of unknown type
+	public static void display(List<?> ll) {
+		for (Object o : ll) {
+			System.out.println(o);
+		}
+	}
+}
