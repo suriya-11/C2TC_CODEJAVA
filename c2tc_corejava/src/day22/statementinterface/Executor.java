2a3a2b135f87bbcfd4e7d8e1f63d089afb33901d
HP <HP@DESKTOP-Q9DA9LP>
2025-08-08 22:55:44 +0530

    day22

---
+++ c2tc_corejava/src/day22/statementinterface/Executor.java
@@ -0,0 +1,20 @@
+package day22.statementinterface;
+
+public class Executor {
+
+	public static void main(String[] args) {
+		StatementInterfaceDemo.showEmp();
+		System.out.println("----------------------------------------------");
+		System.out.println(StatementInterfaceDemo.updateEmpName(5, "Neha"));
+		StatementInterfaceDemo.updateEmpSalary(5, 65000);
+		System.out.println("-------------After Updating Name and Salary------------");
+		StatementInterfaceDemo.showEmp();
+		StatementInterfaceDemo.deleteEmp(5);
+		StatementInterfaceDemo.deleteEmp(101);
+		StatementInterfaceDemo.deleteEmp(102);
+		System.out.println("-----------After Deleting --------------------");
+		StatementInterfaceDemo.showEmp();
+
+	}
+
+}
